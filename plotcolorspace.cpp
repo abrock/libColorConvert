720ddc6fbf31dbd2ed71c3f596434330a9c67348
Alexander Brock <alexander@lunar-orbit.de>
2016-12-03 12:33:22 +0100

    Plot colorspace

--- plotcolorspace.cpp
+++ plotcolorspace.cpp
@@ -23,15 +23,15 @@ int main(void) {
     size_t const steps = 100;
     size_t min_L = 0; size_t max_L = 100;
 
-    double const min_a = -170;
-    double const max_a = 100;
-    double const min_b = -100;
-    double const max_b = 150;
+    double const min_a = -40;
+    double const max_a = 40;
+    double const min_b = -40;
+    double const max_b = 40;
 
     size_t const width = 1000;
     size_t const height = 1000;
-    double min_threshold = .01;
-    double max_threshold = 254.99;
+    double min_threshold = 0.001;
+    double max_threshold = 254.999;
 
     double min_success_a = max_a;
     double max_success_a = min_a;
@@ -55,9 +55,9 @@ int main(void) {
                 auto RGB = ColorConvert::DIN2rgb(vec);
 
                 if (
-                        RGB[0] >= min_threshold && RGB[0] <= max_threshold &&
-                        RGB[1] >= min_threshold && RGB[1] <= max_threshold &&
-                        RGB[2] >= min_threshold && RGB[2] <= max_threshold) {
+                        RGB[0] > min_threshold && RGB[0] < max_threshold &&
+                        RGB[1] > min_threshold && RGB[1] < max_threshold &&
+                        RGB[2] > min_threshold && RGB[2] < max_threshold) {
 #pragma omp critical
                     {
                         frameCol[jj][0] = cv::saturate_cast<uchar>(RGB[2]);
@@ -73,7 +73,10 @@ int main(void) {
             }
         }
         std::cout << "In frame #" << frameNum << " we have " << 100 * static_cast<double>(success_counter) / static_cast<double>(counter) << "% success" << std::endl;
+        std::cout << "a : [" << min_success_a << ", " << max_success_a << "]" << std::endl;
+        std::cout << "b : [" << min_success_b << ", " << max_success_b << "]" << std::endl;
     }
+
 
 #pragma omp parallel for
     for (size_t frameNum = 0; frameNum < steps; frameNum++) {
@@ -102,5 +105,10 @@ int main(void) {
         }
         cv::imwrite(toStringLZ(frameNum, 4) + ".png", frame);
         std::cout << "In frame #" << frameNum << " we have " << 100 * static_cast<double>(success_counter) / static_cast<double>(counter) << "% success" << std::endl;
+        std::cout << "a : [" << min_success_a << ", " << max_success_a << "]" << std::endl;
+        std::cout << "b : [" << min_success_b << ", " << max_success_b << "]" << std::endl;
     }
+    std::cout << "a : [" << min_success_a << ", " << max_success_a << "]" << std::endl;
+    std::cout << "b : [" << min_success_b << ", " << max_success_b << "]" << std::endl;
+
 }
